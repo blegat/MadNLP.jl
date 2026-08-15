be7195cc068cda24e95a07ba7e917feedc18e65b
Benoît Legat <benoit.legat@gmail.com>
2026-08-15 15:26:24 +0000

    Set the quadratic objective with Nonlinear.set_objective
    MOI.Nonlinear.ModelWithQuad no longer implements MOI.set for the objective;
    Nonlinear.set_objective is the single way to set it.

--- ext/MadNLPMOI/MOI_wrapper.jl
+++ ext/MadNLPMOI/MOI_wrapper.jl
@@ -1128,7 +1128,7 @@ function MOI.set(
     },
 }
     # This also clears any objective of the inner nonlinear model.
-    MOI.set(model.quad_data, attr, func)
+    MOI.Nonlinear.set_objective(model.quad_data, func)
     model.solver = nothing
     return
 end
